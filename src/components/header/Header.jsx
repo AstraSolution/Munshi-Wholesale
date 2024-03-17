5b07cf6e3e94fef9c2edc81a956423be944b33e0
Minhajul-alam <alam.minhajul@outlook.com>
2024-03-17 17:31:13 +0600

    Update header UI

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <div className="bg-black py-3">
-      <h1 className="text-white text-center text-xl font-bold">
+    <div className="bg-black py-2">
+      <h1 className="text-white text-center text-sm font-semibold">
         Please Contact with +880 000 000 000 or Web.MunshiWholesale@outlook.com
       </h1>
     </div>
