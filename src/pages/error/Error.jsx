7447235e1908cf7921075ba60da001f509cc268a
Minhajul-alam <alam.minhajul@outlook.com>
2024-03-17 19:37:42 +0600

    Update error page UI

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,7 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 const Error = () => {
   return (
     <div>
-      <h1>404</h1>
+      <div className="flex flex-col justify-center items-center mt-40">
+        <h1 className="text-9xl font-bold">404</h1>
+        <p className="text-sm font-semibold">Opps!!! it&apos;s an error.</p>
+        <NavLink to={"./"}>
+          <button className="bg-black py-2 px-3 text-white rounded-lg mt-3">
+            Go to Home
+          </button>
+        </NavLink>
+      </div>
     </div>
   );
 };
