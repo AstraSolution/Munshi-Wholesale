0d52736d909f5ac2894d4872351631479c0b9a03
Minhajul-alam <alam.minhajul@outlook.com>
2024-03-17 17:03:43 +0600

    tailwind setup and added header section

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="bg-black py-3">
+      <h1 className="text-white text-center text-xl font-bold">
+        Please Contact with +880 000 000 000 or Web.MunshiWholesale@outlook.com
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/routers/Route.jsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../root/Root";
+
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+export default router;
