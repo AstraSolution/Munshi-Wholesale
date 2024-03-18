5aa2eab7111dd64b31b0d54dc3eff0e545a1b1b9
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-03-18 12:37:55 +0600

    added axios public and others

---
+++ src/hooks/axios/useAxiosSecure.jsx
@@ -0,0 +1,40 @@
+
+// import axios from "axios";
+// import { useEffect } from "react";
+// // import { useNavigate } from "react-router-dom";
+// import useAuth from "../auth/useAuth";
+
+// import { useRouter } from "next/navigation";
+
+// export const axiosSecure = axios.create({
+// //   baseURL: "https://boi-binimoy-server.vercel.app/api/v1",
+//   baseURL: "http://localhost:5000/api/v1",
+//   withCredentials: true,
+// });
+
+// const useAxiosSecure = () => {
+//   const { logOut } = useAuth();
+//   // const navigate = useNavigate();
+//   const navigate = useRouter();
+//   useEffect(() => {
+//     axiosSecure.interceptors.response.use(
+//       (res) => {
+//         return res;
+//       },
+//       (error) => {
+//         console.log("error tracked in the interceptor", error);
+//         if (error.response.status === 401 || error.response.status === 403) {
+//           logOut()
+//             .then(() => {
+//               navigate.push("/joinUs");
+//             })
+//             .catch((error) => console.log(error));
+//         }
+//       }
+//     );
+//   }, [logOut, navigate]);
+
+//   return axiosSecure;
+// };
+
+// export default useAxiosSecure;
