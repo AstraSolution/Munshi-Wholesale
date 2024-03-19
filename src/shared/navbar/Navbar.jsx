03e09db7ca0bc89bbb9474eda700dc3cb0509d1e
Minhajul-alam <alam.minhajul@outlook.com>
2024-03-19 12:45:53 +0600

    Update navbar UI

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -1,80 +1,27 @@
 import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 import { PhoneIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { NavLink } from "react-router-dom";
 import Logo from "../logo/Logo";
 
 const Navbar = () => {
-  const navLinks = (
-    <>
-      <li>
-        <NavLink
-          to={"/"}
-          className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? "border-b-2 border-black"
-              : "hover:border-b-2 border-black"
-          }
-        >
-          Home
-        </NavLink>
-      </li>
+  const navLinks = [
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" },
+    { label: "Contact", link: "/contact" },
+    { label: "SignUp", link: "/sign-up" },
+  ];
 
-      <li>
-        <NavLink
-          to={"/about"}
-          className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? "border-b-2 border-black"
-              : "hover:border-b-2 border-black"
-          }
-        >
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={"/contact"}
-          className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? "border-b-2 border-black"
-              : "hover:border-b-2 border-black"
-          }
-        >
-          Contact
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={"/sign-up"}
-          className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? "border-b border-black"
-              : "hover:border-b border-black"
-          }
-        >
-          SignUp
-        </NavLink>
-      </li>
-    </>
-  );
   return (
     <div>
       <div className="bg-black">
-        <div className="max-w-7xl mx-auto py-5">
+        <div className="max-w-7xl mx-auto py-5 px-5">
           <div className="flex justify-between items-center">
             <div className="text-4xl text-white">
               <Logo />
             </div>
 
-            <form className="">
+            <form className="hidden lg:flex items-center">
               <input
                 type="text"
                 placeholder="Search..."
@@ -100,17 +47,58 @@ const Navbar = () => {
       </div>
 
       <div className="bg-yellow-400 py-2">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <div>
-            <ul className="flex justify-around items-center gap-10 text-xl font-semibold">
-              {navLinks}
-            </ul>
+        <div className="max-w-7xl mx-auto px-5">
+          <div className="flex justify-center lg:hidden">
+            <form className="flex items-center">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="py-2 px-4 w-96 rounded-l-lg text-lg focus:outline-none"
+              />
+              <button
+                type="submit"
+                // className="bg-black text-white py-2 px-3 rounded-r-lg text-lg font-semibold"
+                className=""
+              >
+                <MagnifyingGlassIcon className="size-11 py-2 px-3 bg-black text-white rounded-r-lg" />
+              </button>
+            </form>
           </div>
-          <div className="flex items-center gap-3">
-            <PhoneIcon className="size-9" />
-            <div>
-              <h1>Customer Support:</h1>
-              <p>+880 000 000 000</p>
+
+          <div className="hidden lg:flex justify-between items-center px-5">
+            <ul className="flex justify-around items-center gap-10 text-xl font-semibold">
+              {navLinks?.map((navLink, key) => (
+                <li key={key}>
+                  <NavLink
+                    to={navLink.link}
+                    className={({ isActive, isPending }) =>
+                      isPending
+                        ? "pending"
+                        : isActive
+                        ? "pb-1 border-b-2 border-black"
+                        : "pb-1 hover:border-b-2 border-black transition-all duration-300"
+                    }
+                  >
+                    {navLink.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+
+            <div className="flex items-center gap-3">
+              <PhoneIcon className="size-9" />
+              <div>
+                <h1 className="font-semibold">Customer Support:</h1>
+                <p className="text-xs">
+                  <span className="hover:underline cursor-pointer">
+                    +880 000 000 000
+                  </span>{" "}
+                  &{" "}
+                  <span className="hover:underline cursor-pointer">
+                    example@gmail.com
+                  </span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
