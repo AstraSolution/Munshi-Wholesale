015fc61861d9f1c922ca6fcee413ad15098b2718
Your Name <you@example.com>
2024-03-19 12:13:49 +0600

    card refining done

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -7,13 +7,14 @@ const Card = () => {
       <div className="container-card">
         <div className="card">
           <div className="imgBx">
-            <img src="https://i.ibb.co/1vh8BKh/mpplay17a-removebg-preview.png" alt="Nike Shoes" />
+            <img src="https://i.ibb.co/1vh8BKh/mpplay17a-removebg-preview.png" alt="Screw" />
           </div>
           <div className="contentBx">
-            <h2 className='text-white'>Screw</h2>
-            <div className="flex justify-evenly">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buy Now</button>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buy Now</button>
+            <h2 className='text-white text-center -mt-6'>Title: Screw</h2>
+           
+            <div className="flex justify-evenly mt-4">
+            <button className="bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+            <button className="bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded">View Details</button>
             </div>
           </div>
         </div>
