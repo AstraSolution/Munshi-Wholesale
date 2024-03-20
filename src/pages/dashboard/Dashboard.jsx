eef81ac70c6a1f08c1d45ed4b7313ea4b00f88c4
Minhajul-alam <alam.minhajul@outlook.com>
2024-03-20 10:14:03 +0600

    Added dashbord contant div

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -28,9 +28,9 @@ const Dashboard = () => {
         <div className="flex justify-between items-center py-2 px-5">
           <button onClick={toggleSlider}>
             {isSliderOpen ? (
-              <ChevronLeftIcon className="size-8 bg-yellow-400 p-1 rounded-full" />
+              <ChevronLeftIcon className="size-8 bg-yellow-400 hover:bg-yellow-500 p-1 rounded-full transition-all duration-300" />
             ) : (
-              <ChevronRightIcon className="size-8 bg-yellow-400 p-1 rounded-full" />
+              <ChevronRightIcon className="size-8 bg-yellow-400 hover:bg-yellow-500 p-1 rounded-full transition-all duration-300" />
             )}
           </button>
 
@@ -42,7 +42,7 @@ const Dashboard = () => {
               onClick={toggleDropdown}
             />
             {isDropdownOpen && (
-              <div className="absolute right-0 flex flex-col items-center gap-2 bg-yellow-500 w-44 p-3 rounded-md mt-1 text-white">
+              <div className="absolute right-0 flex flex-col items-center gap-2 bg-yellow-500 w-44 p-3 rounded-md mt-1 text-white z-10">
                 <h1 className="text-lg font-semibold">Resel Mia</h1>
                 <button className="bg-black py-1 px-2 font-semibold rounded-md shadow-md hover:shadow">
                   Logout
@@ -51,9 +51,14 @@ const Dashboard = () => {
             )}
           </div>
         </div>
+
+        <div className="p-2">
+          <h1>Dashboard content</h1>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Dashboard;
+
