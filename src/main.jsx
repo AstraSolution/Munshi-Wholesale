7e5306b49f41fe2a7f8490dc78947553e1cd6f4c
parvejme24 <mdparvejme24@gmail.com>
2024-03-21 19:51:30 +0600

    added auth provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routers/Route";
 import AuthProvider from "./AuthProvider/AuthProvider";
 
-
 import TranstackProvider from "./providers/TranstackProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TranstackProvider>
-      <RouterProvider router={router} />
-    </TranstackProvider>  
-
+    <AuthProvider>
+      <TranstackProvider>
+        <RouterProvider router={router} />
+      </TranstackProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
