d620288cc8536ad2718d3cf37c0cbef207ed8589
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-05-03 20:34:36 +0600

    added hero section and category sections

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,19 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Shared/Navbar/Navbar";
 import Footer from "../Components/Shared/Footer/Footer";
+import Hero from "../Components/HomeComonents/Hero/Hero";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Category from "../Components/HomeComonents/Category/Category";
+import Category2 from "../Components/HomeComonents/Category/Category2";
 
 export default function MainLayout() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Category />
+      <Category2 />
       <Outlet />
       <Footer />
     </div>
