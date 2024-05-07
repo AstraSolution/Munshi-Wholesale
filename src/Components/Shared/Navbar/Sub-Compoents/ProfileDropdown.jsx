bf4cd55b5e5c62623d7e8c51a51ae564696174d2
Md Parvej <mdparvejme24@gmail.com>
2024-05-07 23:50:21 +0600

    update error page

--- src/Components/Shared/Navbar/Sub-Compoents/ProfileDropdown.jsx
+++ src/Components/Shared/Navbar/Sub-Compoents/ProfileDropdown.jsx
@@ -1,26 +1,38 @@
-import { UserCircleIcon, UserIcon } from "@heroicons/react/outline";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { UserCircleIcon } from "@heroicons/react/outline";
+import React, { useState, useEffect } from "react";
 import useAuth from "../../../../Hooks/useAuth";
 import { Button } from "@material-tailwind/react";
-import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa";
 import { AiOutlineAppstore } from "react-icons/ai";
 import { FiBox } from "react-icons/fi";
 import { LuUserSquare2 } from "react-icons/lu";
 import { IoSettingsOutline } from "react-icons/io5";
+import DropdownItem from "./DropdownItem";
 
 const Dropdown = () => {
   const { user, logOut } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
-  console.log(user);
+  const [activeStatus, setActiveStatus] = useState(false);
 
+  // handle logout
   const handleLogOut = () => {
     logOut();
   };
 
+  // toggle dropdown
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  // active status change
+  useEffect(() => {
+    if (user) {
+      const interval = setInterval(() => {
+        setActiveStatus((prevStatus) => !prevStatus);
+      }, 2000);
+      return () => clearInterval(interval);
+    }
+  }, [user]);
 
   return (
     <div className="relative inline-block text-left z-50">
@@ -30,18 +42,28 @@ const Dropdown = () => {
           onClick={toggleDropdown}
           aria-haspopup="true"
           aria-expanded="true"
-          className="mt-2"
+          className="mt-2 relative"
         >
-          {user && user.photoURL ? (
-            <img
-              className="w-10 h-10 rounded-full border-2 border-gray-500"
-              src={user.photoURL}
-            />
-          ) : (
-            <UserCircleIcon className="h-9 w-9 text-gray-600" />
-          )}
-
-          {/* <img className="w-8" src={USER_ICON} alt="" /> */}
+          <div className="relative">
+            {/* User profile image */}
+            {user && user.photoURL ? (
+              <img
+                className="w-10 h-10 rounded-full border-2 border-gray-500"
+                src={user.photoURL}
+                alt="User Profile"
+              />
+            ) : (
+              <UserCircleIcon className="h-9 w-9 text-gray-600" />
+            )}
+            {/* Active status indicator */}
+            {user && (
+              <div
+                className={`absolute bottom-0 right-0 ${
+                  activeStatus ? "bg-green-500" : "bg-gray-500"
+                } w-3 h-3 border-2 border-white rounded-full animate-ping`}
+              />
+            )}
+          </div>
         </button>
       </div>
 
@@ -58,48 +80,40 @@ const Dropdown = () => {
               <div className="p-6 text-gray-600" role="none">
                 <ul className="space-y-4">
                   <li className="text-center text-xl font-bold text-red-500 -mb-3">
-                    {user && user.displayName}
+                    {user.displayName}
                   </li>
-                  <li className="text-center font-semibold text-gray-600">
-                    {user && user.email}
-                  </li>
+                  <li className="text-center font-semibold">{user.email}</li>
                   <li>
-                    {user && (
-                      <hr className="border border-gray-300 w-3/4 mx-auto mb-4" />
-                    )}
+                    <hr className="border border-gray-300 w-3/4 mx-auto mb-4" />
                   </li>
-                  <li className="flex items-center gap-1 font-semibold">
-                    <AiOutlineAppstore size={23} className="min-w-max" />
-                    <Link to={"/dashboard"}>Dashboard</Link>
-                  </li>
-                  <li className="flex items-center gap-1 font-semibold">
-                    <FiBox size={23} className="min-w-max" />
-                    <Link to={"/dashboard/my_order"}>My Orders</Link>
-                  </li>
-                  <li className="flex items-center gap-1 font-semibold">
-                    <LuUserSquare2 size={23} className="min-w-max" />
-                    <Link to={"/dashboard/profile"}>Profile</Link>
-                  </li>
-                  <li className="flex items-center gap-1 font-semibold">
-                    <IoSettingsOutline size={23} className="min-w-max" />
-                    <Link to={"/dashboard/my_order"}>Settings</Link>
-                  </li>
+                  {/* Menu items */}
+                  <DropdownItem icon={<AiOutlineAppstore />} to="/dashboard">
+                    Dashboard
+                  </DropdownItem>
+                  <DropdownItem icon={<FiBox />} to="/dashboard/my_order">
+                    My Orders
+                  </DropdownItem>
+                  <DropdownItem
+                    icon={<LuUserSquare2 />}
+                    to="/dashboard/profile"
+                  >
+                    Profile
+                  </DropdownItem>
+                  <DropdownItem
+                    icon={<IoSettingsOutline />}
+                    to="/dashboard/my_order"
+                  >
+                    Settings
+                  </DropdownItem>
+                  {/* Logout button */}
                   <li>
-                    {user ? (
-                      <Button
-                        onClick={handleLogOut}
-                        color="red"
-                        className="capitalize flex justify-center items-center text-md gap-2 w-full "
-                      >
-                        Logout <FaArrowRightFromBracket />
-                      </Button>
-                    ) : (
-                      <Link to={"/login"}>
-                        <Button className="w-full" color="red">
-                          Login
-                        </Button>
-                      </Link>
-                    )}
+                    <Button
+                      onClick={handleLogOut}
+                      color="red"
+                      className="capitalize flex justify-center items-center text-md gap-2 w-full "
+                    >
+                      Logout <FaArrowRight />
+                    </Button>
                   </li>
                 </ul>
               </div>
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,23 +1,32 @@
 import React from "react";
+import Navbar from "../../Components/Shared/Navbar/Navbar";
+import Footer from "../../Components/Shared/Footer/Footer";
+import { Button } from "@material-tailwind/react";
 
 const EmptyComponent = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="max-w-sm">
-        <img
-          src="https://staticmania.cdn.prismic.io/staticmania/ed90f683-c1df-4bad-afa4-65ce4c65287e_Property+1%3DSpaceship_+Property+2%3DMd.svg"
-          className="w-[234] h-[350] mx-auto"
-          alt="404"
-        />
-        <h1 className="text-3xl font-bold text-center">404 Not Found</h1>
-        <p className="mt-4 text-center">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry.
-        </p>
-        <div className="mt-6 flex justify-center">
-          <button className="border px-4 py-3 rounded-md shadow-sm">Go to home</button>
+    <div>
+      <Navbar />
+      <div className="bg-[url('https://i.ibb.co/M56D6xg/intro-section-pattern.png')]">
+        <div className="flex flex-col items-center justify-center py-28">
+          <div className="max-w-sm">
+            <img
+              src="https://staticmania.cdn.prismic.io/staticmania/ed90f683-c1df-4bad-afa4-65ce4c65287e_Property+1%3DSpaceship_+Property+2%3DMd.svg"
+              className="w-[234] h-[350] mx-auto"
+              alt="404"
+            />
+            <h1 className="text-3xl font-bold text-center text-red-500">404 Not Found</h1>
+            <p className="mt-4 text-center">
+              Sorry, the requested page isn't found. Perhaps a typo or a broken
+              link. Redirecting back to safety.
+            </p>
+            <div className="mt-6 flex justify-center">
+              <Button color="amber" className="text-white">Go Home</Button>
+            </div>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
