aba141765a8ec5b99a9b68e2f2cb9cab33853a2a
Md Parvej <mdparvejme24@gmail.com>
2024-05-08 16:13:12 +0600

    added service section in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
-
 import Category from "../../Components/HomeComonents/Category/Category";
 import Category2 from "../../Components/HomeComonents/Category/Category2";
 import Hero from "../../Components/HomeComonents/Hero/Hero";
 import Offer from "../../Components/HomeComonents/Offer/Offer";
+import Services from "../../Components/HomeComonents/Services/Services";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Services />
       <Category />
       <Category2 />
       <Offer />
