1f2f3b6223e1c08e701341989f3e9c8b4de60099
siyam79 <siyamahmed3827@gmail.com>
2024-05-10 18:21:20 +0600

    user dashboard homr page requmented product show

--- src/pages/user_dashboard/Dashboard_Home/Header.jsx
+++ src/pages/user_dashboard/Dashboard_Home/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MdOutlineProductionQuantityLimits } from "react-icons/md";
 import useWishlistProducts from '../../../Hooks/useWishlistProducts';
 import { Link } from 'react-router-dom';
+import useGetMyCarts from '../../../Hooks/useGetMyCarts';
 
 
 const Header = () => {
@@ -9,6 +10,8 @@ const Header = () => {
     const [wishlistProduct] = useWishlistProducts();
     const wishlistItems = wishlistProduct?.length;
 
+    const { myCarts } = useGetMyCarts();
+    const cartItems = myCarts?.length;
 
     return (
 
@@ -27,6 +30,16 @@ const Header = () => {
                 </Link>
 
 
+                <div className="bg-white  shadow-lg rounded-md flex items-center justify-between  py-6 px-3  text-gray-800 font-medium group">
+                    <div className="flex justify-center items-center w-14 h-14 bg-[#FF9D00]  rounded-full transition-all duration-300 transform group-hover:rotate-12">
+                        <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current  text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
+                    </div>
+                    <div className="text-right">
+                        <p className="text-2xl"> {cartItems} </p>
+                        <p>My carts </p>
+                    </div>
+                </div>
+
                 <div className="bg-white  shadow-lg rounded-md flex items-center justify-between  py-6 px-3   text-gray-800 font-medium group">
                     <div className="flex justify-center items-center w-14 h-14 bg-[#FF9D00] rounded-full transition-all duration-300 transform group-hover:rotate-12">
                         <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
@@ -34,15 +47,6 @@ const Header = () => {
                     <div className="text-right">
                         <p className="text-2xl">557</p>
                         <p>Orders</p>
-                    </div>
-                </div>
-                <div className="bg-white  shadow-lg rounded-md flex items-center justify-between  py-6 px-3  text-gray-800 font-medium group">
-                    <div className="flex justify-center items-center w-14 h-14 bg-[#FF9D00]  rounded-full transition-all duration-300 transform group-hover:rotate-12">
-                        <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current  text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
-                    </div>
-                    <div className="text-right">
-                        <p className="text-2xl">$11,257</p>
-                        <p>Buy</p>
                     </div>
                 </div>
                 <div className="bg-white  shadow-lg rounded-md flex items-center justify-between  py-6 px-3  text-gray-800 font-medium group">
