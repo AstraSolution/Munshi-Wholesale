84675c787b9f0c5b4782432417d436ba92351cb4
taiatulislam <taiatulislamapon@gmail.com>
2024-05-16 22:04:27 +0600

    route error solved

--- src/Hooks/useAllProduct.jsx
+++ src/Hooks/useAllProduct.jsx
@@ -1,6 +1,8 @@
 import useAxiosPublic from "./useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 
+// route set
+
 const useAllProduct = (page, limit, searchItems) => {
   const axiosPublic = useAxiosPublic();
 
