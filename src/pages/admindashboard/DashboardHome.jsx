f46a21c5955c9d79e0a319bb77ea69b61ad6d326
Your Name <you@example.com>
2024-05-20 17:55:16 +0600

    charts updated

--- src/pages/admindashboard/DashboardHome.jsx
+++ src/pages/admindashboard/DashboardHome.jsx
@@ -1,4 +1,4 @@
-import AllAnalyticsChart from "./Charts/AllAnalyticsChart";
+// import AllAnalyticsChart from "./Charts/AllAnalyticsChart";
 import BrandChart from "./Charts/BrandChart";
 import CategoryChart from "./Charts/CategoryChart";
 import DashboardStats from "./DashboardStats";
@@ -8,15 +8,15 @@ const DashboardHome = () => {
         <div>
             <DashboardStats></DashboardStats>
 
-            <div className="lg:flex justify-evenly">
-                <div className="lg:w-[60%] ">
+            <div className="lg:flex justify-evenly lg:ml-5 mt-5 mb-5">
+                <div className="w-full lg:w-[60%]  ">
                     <BrandChart></BrandChart>
                 </div>
-                <div className="w-full lg:w-[33%]">
+                <div className=" lg:w-[37%] md:w-[50%] lg:ml-0 md:ml-52">
                     <CategoryChart></CategoryChart>
                 </div>
             </div>
-            <AllAnalyticsChart></AllAnalyticsChart>
+            {/* <AllAnalyticsChart></AllAnalyticsChart> */}
             <TopProducts></TopProducts>
         </div>
     );
